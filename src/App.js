a478f420a821dc48ae34b90fd9b503640f21e8b6
xencodes <xencodes@icloud.com>
2021-02-24 13:15:08 +0530

    random numbers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import KeyboardEventHandler from "react-keyboard-event-handler";
+import "./App.css";
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const letters = (() => {
+  const caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+  return caps.concat(caps.map((letter) => letter.toLowerCase()));
+})();
 
 function App() {
+  const [letter, setLetter] = useState(1);
+
+  const UpdateLetter = () => setLetter(getRandomInt(65, 90));
+
+  useEffect(() => {
+    setTimeout(UpdateLetter, 1000);
+  }, [letter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Letter">
+        <KeyboardEventHandler
+          handleKeys={letters}
+          onKeyEvent={(key, e) => setLetter(key)}
+        />
+        <h1>{String(letter)}</h1>
       </header>
     </div>
   );
